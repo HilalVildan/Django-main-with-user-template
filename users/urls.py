c01a955ceb52177694c734ee438ba036647bbdbd
HilalVildan <hilalsarioglu96@gmail.com>
2023-01-19 17:07:01 +0100

    users added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import logout, UserView
 
+router = routers.DefaultRouter()
+router.register('users', UserView)
 
 urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls')),
+    path('', include(router.urls)),
+    path('login/', obtain_auth_token),
+    path('logout/', logout), #logout
 ]
